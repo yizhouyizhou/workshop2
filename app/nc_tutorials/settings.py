f0519aef3020397b08f1bf7965f14b20cafdc8ca
Yi Zhou <viczhyi@gmail.com>
2022-08-20 11:57:19 -0400

    fixing allwoed host bug

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'ec2-44-205-41-47.compute-1.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
+    'ec2-44-205-51-122.compute-1.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
 ]
 
 
